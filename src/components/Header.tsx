73dc6a10741e3449dc97b3f35d6d47f9ff66db17
Luke Davies <luke.davies@junitech.com>
2025-02-26 02:01:32 +0000

    improved media queries for phones

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import "./Header.css";
 
 const Header: React.FC = () => {
   const [isScrolled, setIsScrolled] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,20 +14,10 @@ const Header: React.FC = () => {
 
   const leftNavItems = ["FEATURES", "SHOWCASE"];
   const rightNavItems = ["COMMUNITY", "CONNECT"];
-  const allNavItems = [...leftNavItems, ...rightNavItems];
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
 
   return (
-    <header
-      className={`header ${isScrolled ? "scrolled" : ""} ${
-        isMobileMenuOpen ? "mobile-nav-open" : ""
-      }`}
-    >
+    <header className={`header ${isScrolled ? "scrolled" : ""}`}>
       <div className="container">
-        {/* Desktop Navigation */}
         <nav>
           <ul className="nav-list left">
             {leftNavItems.map((item) => (
@@ -61,38 +50,6 @@ const Header: React.FC = () => {
             ))}
           </ul>
         </nav>
-
-        {/* Mobile Navigation */}
-        <div className="mobile-nav-container">
-          <a href="/" className="logo">
-            <img
-              src="/LS_LOGO.png"
-              alt="Your Life RP Logo"
-              width={150}
-              height={150}
-            />
-          </a>
-
-          <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
-            {isMobileMenuOpen ? "✕" : "☰"}
-          </div>
-
-          {isMobileMenuOpen && (
-            <ul className="mobile-nav">
-              {allNavItems.map((item) => (
-                <li key={item} className="mobile-nav-item">
-                  <a
-                    href={`#${item.toLowerCase()}`}
-                    className="mobile-nav-link"
-                    onClick={() => setIsMobileMenuOpen(false)}
-                  >
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
       </div>
     </header>
   );
