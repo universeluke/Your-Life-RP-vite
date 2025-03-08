a960b8f61cab0431d1749d425edac58338393bc4
Luke Davies <luke.davies@junitech.com>
2025-03-08 18:34:25 +0000

    fixes react import

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Shield, Users, Briefcase, Map, Zap, Crosshair } from "lucide-react";
 import "./Features.css";
 
