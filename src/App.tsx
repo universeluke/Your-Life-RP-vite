3a64168b6fd17fba12e49c7baa1e1946a5d73bb9
Luke Davies <luke.davies@junitech.com>
2025-03-09 02:55:30 +0000

    fix deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Community from "./components/Community";
-import JoinNow from "./components/JoinNow";
+// import JoinNow from "./components/JoinNow";
 import Footer from "./components/Footer";
 import Carousel from "./components/Carousel";
 import ScrollingImageBelt from "./components/ScrollingImageBelt";
